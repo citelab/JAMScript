e2c710a64f92cd8ff58ca061ba839c67e208a6b8
Muthucumaru Maheswaran <mahes25@gmail.com>
2015-05-11 13:30:34 -0400

    A new version..

--- JAMScript/ccompiler.js
+++ JAMScript/ccompiler.js
@@ -1,22 +1,37 @@
 require('ometa-js')
 var CParser = require('./cparser.ojs')
 
-var readline = require('readline')
-
-var rl = readline.createInterface(process.stdin, process.stdout);
-rl.setPrompt('CParser > ');
-rl.prompt();
+fs = require('fs')
 
 
-rl.on('line', function(line) {
-	if (line === "right") rl.close();
+// Command line parsing for the correct arguments
+// Should be replaced with the 'getopt' NPM module
+//
+// This is very ugly stuff..
+//
+if (process.argv.length < 3) {
+    console.log("Invalid number of args");
+    process.exit(1);
+}
 
-	try {
-	    tree = CParser.matchAll(line, 'top')
-	} catch(e) {
-	    console.log("\t\t\t\t ERROR! Invalid Input");
-	}
-	rl.prompt();
-    }).on('close',function() {
-	    process.exit(0);
-	});
+var fpath = process.argv[2];
+
+fs.exists(fpath, function(exists) {
+        if (exists) {
+            console.log("it's there");
+        } else {
+            console.log("no file");
+            process.exit(1);
+        }
+    });
+
+fs.readFile(fpath, 'utf8', function (err,data) {
+   	if (err) {
+      return console.log(err);
+   }
+        try {
+            CParser.parse(data);
+        } catch (err) {
+            console.log(err);
+        }
+   });
