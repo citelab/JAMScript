649272c82823fd53cc11677b5d7dc152f683238c
Muthucumaru Maheswaran <mahes25@gmail.com>
2015-05-16 21:13:37 -0400

    Added more support files before the testing starts.

---
+++ JAMScript/C/index.js
@@ -0,0 +1,17 @@
+require('../ometa');
+
+var parser     = require('./grammars/c_parser.ojs');
+
+C = module.exports = {
+
+    parse:      parser.parse,
+    translate:  translator.translate,
+    compile: function(input) {
+        return C.translate(
+            C.parse(input));
+  },
+
+    parser:     parser,
+    translator: translator,
+    nodes:      require('./nodes.js')
+}
