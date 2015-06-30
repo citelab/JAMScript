e241d35d18b9edca3bf5518be276eed7dfe8662b
Muthucumaru Maheswaran <mahes25@gmail.com>
2015-06-30 08:45:03 +0530

    JAMCTranslator coding complete. Testing next.

--- .cache/jamc_parser.ojs.1434760501000.js
+++
@@ -1,8 +0,0 @@
-{{var CParser=require("../../c/grammars/c_parser.ojs");var __=require("../nodes.js")}var JAMCParser=CParser.inherit({_grammarName: "JAMCParser",
-
-/*
-
-
-    function_def    = "jamdef" | ^function_def
-*/
-"function_def":function(){return this._or((function(){return (function(){undefined;return this._or((function(){return (function(){return this._applyWithArgs("token","jamdef")}).call(this)}),(function(){return (function(){return CParser._superApplyWithArgs(this,'function_def')}).call(this)}))}).call(this)}))}});(JAMCParser["parse"]=(function (input){return JAMCParser.matchAll(input,"topLevel")}));(module["exports"]=JAMCParser)}
