6fea81e8b41bb2043d278d54c3bc7cc6300255a3
Robert Wenger <robwenger@gmail.com>
2015-08-26 22:29:04 -0400

    Fix for hex handling

--- tests/tcc_tests/03_struct.c
+++ tests/tcc_tests/03_struct.c
@@ -1,1 +1,31 @@
-int a = 0x73;
+#include <stdio.h>
+
+struct fred
+{
+   int boris;
+   int natasha;
+};
+
+int main() 
+{
+   struct fred bloggs;
+   struct fred jones[2];
+
+   bloggs.boris = 12;
+   bloggs.natasha = 34;
+
+   printf("%d\n", bloggs.boris);
+   printf("%d\n", bloggs.natasha);
+
+   jones[0].boris = 12;
+   jones[0].natasha = 34;
+   jones[1].boris = 56;
+   jones[1].natasha = 78;
+
+   printf("%d\n", jones[0].boris);
+   printf("%d\n", jones[0].natasha);
+   printf("%d\n", jones[1].boris);
+   printf("%d\n", jones[1].natasha);
+
+   return 0;
+}
