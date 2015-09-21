8b9a5af826bbeb34c9ae8694b6b5e062fc8f367f
Robert Wenger <robwenger@wpa036132.wireless.mcgill.ca>
2015-09-21 15:24:41 -0400

    Start handler checks

--- tests/test_jam.c
+++ tests/test_jam.c
@@ -1,10 +1,4 @@
-jamdef void test(int b) {
-	int onload() {
-		int a = 1;
-		return a;
-	};
-	int oncomplete() {
-		var b = 2;
-		return b;
-	};
+jamsync void test(int b) {
+	int b = 1;
+	return b;
 }
