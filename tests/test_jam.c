60fc285fb414551002576db40e8deb852390eb22
Robert Wenger <robwenger@Roberts-MacBook-Pro.local>
2015-10-04 23:00:11 -0400

    Add load and error handlers

--- tests/test_jam.c
+++ tests/test_jam.c
@@ -1,13 +1,41 @@
-jamsync int test(){
-	var a;
-	return a;
+jamdef void test(){
+	void onload() {
+		var a;
+		console.log("Hello World!");
+	};
+}
+
+jamdef void bad(){
+	void onload() {
+		var a;
+		blah("Hello World!");
+	};
+	void onerror() {
+		printf("Error received\n");
+	};
 }
 
 int main() {
-	printf("%d\n", test_sync());
-}
+	int code;
+    int loop = 1;
+    while(loop) {
+        printf("Your choice: \n \
+                1 - Execute hello \n \
+                2 - Trigger error \n \
+                3 - Quit \n");
 
-int test2() {
-	main();
+        scanf("%d", &code);
+        switch (code) {
+            case 1:
+                test_load();
+                break;
+            case 2:
+                bad_load();
+                break;
+            case 3:
+                loop = 0;
+                break;
+        }
+    }
+    return 0;
 }
-
