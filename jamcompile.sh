231d740bf8ffecb5cc9ba672d30a4382ebfdfb92
Robert Wenger <robwenger@wpa036067.wireless.mcgill.ca>
2015-12-10 12:19:03 -0500

    Add operating system check

--- jamcompile.sh
+++ jamcompile.sh
@@ -4,5 +4,10 @@ gcc-5 -E -P -std=iso9899:199409 test_jam.c > pre_jam.c
 cd ..
 node jamout
 cd tests
-gcc -std=iso9899:199409 jamlib.a jamout.c
+if [ "$(uname -s)" == "Darwin" ]; then
+    gcc -std=iso9899:199409 jamlib.a jamout.c
+elif [ "$(uname -s)" == "Linux" ]; then
+    gcc -std=iso9899:199409  ../lib/jamlib/*.o jamout.c -lpthread -lbsd
+fi
+
 cd ..
