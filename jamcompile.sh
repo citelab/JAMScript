2a89c3d10004d3534bdbeff3a87b8ce1b16e7c89
Robert Wenger <robwenger@Roberts-MacBook-Pro.local>
2015-12-21 22:33:56 -0500

    Type checker updates

--- jamcompile.sh
+++ jamcompile.sh
@@ -4,6 +4,6 @@ gcc-5 -E -P -std=iso9899:199409 test_jam.c > pre_jam.c
 cd ..
 node jamout
 cd tests
-flow annotated_jamout.js
+cat annotated_jamout.js | flow check-contents
 gcc -std=iso9899:199409 jamlib.a jamout.c
 cd ..
