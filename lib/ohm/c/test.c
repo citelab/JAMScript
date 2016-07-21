ded8ff58136a10de9930dfe8d0eff0c974d1c621
Robert Wenger <robwenger@gmail.com>
2016-07-21 13:59:03 -0400

    Add ohm c and js grammars

---
+++ lib/ohm/c/test.c
@@ -0,0 +1,87 @@
+int test(int *a, double c, char *, ...) {
+    extern long a =3 ;
+    long long a;
+    short int a;
+    long long int a;
+    long long unsigned int a;
+    void * test;
+    goto test;
+    continue;
+    break;
+    return;
+    return 4;
+    while(a < 2) {
+        return 3;
+    }
+    while    (a > 3);
+    do {
+        x = 1;
+    } while(x < 2);
+    for(i = 0; i<3; i++ ){ 
+        a = 2;
+    }
+    if(a == 2) {
+        a = 3;
+    }
+    if(a!=2) {
+        a = 3;
+    } else {
+        a = 2;
+    }
+    if(a!=2)
+        a = 1;
+    else 
+        a=4;
+    switch(a) {
+        case 3:
+            a = 4;
+            break;
+        default:
+            break;
+    }
+    test: a = 3;
+    3*3;
+    ;
+    int a =    3;
+
+
+    struct Books {
+       int   book_id;
+       float test;
+       int   a, b;
+    } book;  
+
+    enum Day {
+        saturday,
+        sunday = 0,
+        monday,
+        tuesday,
+        wednesday,
+        thursday,
+        friday
+    } workday;
+    enum DAY today = wednesday;
+    a?b:c;
+
+    a = 0;
+    a = 1;
+    float b = 2.2;
+    char c = 'a';
+    a = 0x3;
+    a = 0xab;
+    a = 0XFAu;
+    ++a;
+    a++;
+    int a[100];
+    int a[100][100];
+    a = a[2   ][3]*4;
+    a = a.     b;
+    printf("%i\n",     a(2,3)*   4);
+    a = (  3*4  )  +2;
+    MY_TYPE a = { 
+        .flag = true,
+     .value = 123,
+      .stuff = 0.456 };
+    int x[] = {1,2,3};
+}
+
