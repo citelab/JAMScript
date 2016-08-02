4f1acf7db9e154c677506b010f487f7ff93e705d
Xiru <xiruzhu@yahoo.com>
2016-08-02 12:17:16 -0400

    New stuff

---
+++ lib/jamlib/c_core/testers/Xiru_JAM_Tests/jdata_test.c
@@ -0,0 +1,35 @@
+#include "jam.h"
+#include "jdata.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void msg_rcv_custom(void *j){
+  jbroadcaster *x = (jbroadcaster *)j;
+  printf("%s = %s\n", x->key, x->data);
+}
+
+void jam_run_app(void *arg){
+  jamstate_t *js = arg;
+  jdata_get_server_ip(arg);
+  jshuffler *j = jshuffler_init(0, "Life", NULL);
+  jshuffler_push(j, "First run ...");
+  sleep(1);
+  jshuffler_poll(j);
+  sleep(1);
+  jshuffler_poll(j);
+  jshuffler_push(j, "Second run ...");
+  printf("Hey ... \n");
+}
+
+void taskmain(int argc, char **argv)
+{
+    jamstate_t *js = jam_init();
+    // Start the event loop in another thread.. with cooperative threads.. we
+    // to yield for that thread to start running
+    taskcreate(jam_event_loop, js, STACKSIZE);
+
+    // create the application runner
+    taskcreate(jam_run_app, js, STACKSIZE);
+    printf("Commencing JAM operation \n");
+}
--- lib/jamlib/c_core/testers/Xiru_JAM_Tests/res_node.c
+++ lib/jamlib/c_core/testers/Xiru_JAM_Tests/res_node.c
@@ -5,35 +5,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-void msg_rcv_custom(redisAsyncContext *c, void *reply, void *privdata){
-  redisReply *r = reply;
+void msg_rcv_custom(jbroadcaster *j){
   char *result;
-  if (reply == NULL) return;
-  if (r->type == REDIS_REPLY_ARRAY) {
-      for (int j = 2; j < r->elements; j++) {
-          if(r->element[j]->str != NULL){
-            duk_context *ctx = duk_create_heap_default();
-            printf("Message Received: %s\n",r->element[j]->str);
-            duk_eval_string(ctx, r->element[j]->str);
-            result = strdup(duk_get_string(ctx, -1));
-            jdata_log_to_server("results",result, jdata_default_msg_received);
-            free(result);
-            printf("result is: %s\n", duk_get_string(ctx, -1));
-            duk_pop(ctx);
-            duk_destroy_heap(ctx);
-        }
-      }
-  }
+  duk_context *ctx = duk_create_heap_default();
+  printf("Message Received: %s\n", get_jbroadcaster_value(j));
+  duk_eval_string(ctx, get_jbroadcaster_value(j));
+  result = strdup(duk_get_string(ctx, -1));
+  jdata_log_to_server("results",result, jdata_default_msg_received);
+  free(result);
+  printf("result is: %s\n", duk_get_string(ctx, -1));
+  duk_pop(ctx);
+  duk_destroy_heap(ctx);
 }
 
 void jam_run_app(void *arg){
   jamstate_t *js = arg;
-  char buf[256];
-  int len = 0;
-  redisReply *r;
-  printf("Device ID %s\n", js->cstate->conf->device_id);
-  jdata_subscribe_to_server(js->cstate->conf->device_id, msg_rcv_custom, NULL, NULL);
   jdata_get_server_ip(arg);
+  printf("Device ID %s\n", js->cstate->conf->device_id);
+  jbroadcaster_init(JBROADCAST_STRING, js->cstate->conf->device_id, msg_rcv_custom);
 }
 
 void taskmain(int argc, char **argv)
