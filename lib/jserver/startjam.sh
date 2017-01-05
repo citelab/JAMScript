14a1c49f0c4d8234228c1775e2fbddc390b201fd
Muthucumaru Maheswaran <mahes25@gmail.com>
2017-01-05 15:03:38 -0500

    Script to start a collection of nodes - done.

--- lib/jserver/startjam.sh
+++ lib/jserver/startjam.sh
@@ -1,26 +1,51 @@
-#!/bin/sh
-
-# Start the cloud server
-node jamlib.js --conf=cloud.conf --app=ltop --cloud --port=3883 &
-echo "Started the cloud server"
-
-# Start the fog brokers
-node jamlib.js --conf=fog1.conf --app=ltop --fog --port=2883 &
-node jamlib.js --conf=fog2.conf --app=ltop --fog --port=2884 & 
-node jamlib.js --conf=fog3.conf --app=ltop --fog --port=2885 &
-echo "Started the fog brokers"
-
-# Start the device brokers
-node jamlib.js --conf=dev1.conf --app=ltop  --port=1883 &
-node jamlib.js --conf=dev2.conf --app=ltop  --port=1884 &
-node jamlib.js --conf=dev3.conf --app=ltop  --port=1885 &
-node jamlib.js --conf=dev4.conf --app=ltop  --port=1886 &
-node jamlib.js --conf=dev5.conf --app=ltop  --port=1887 &
-node jamlib.js --conf=dev6.conf --app=ltop  --port=1888 &
-node jamlib.js --conf=dev7.conf --app=ltop  --port=1889 &
-echo "Started the device brokers"
+#!/bin/bash
 
 
 
+function startmosquitto () {
+    local port=$1;
+    $(/usr/local/sbin/mosquitto -p $port -d ) &
+    sleep 1;
+}
 
 
+function findmosquitto () {
+    local port=$1;
+    echo $(ps axwu | grep mosquitto - | grep $port - | wc -l)
+}
+
+
+# Starting the cloud
+
+startmosquitto 3883
+res=$(findmosquitto 3883)
+if [ $res = 1 ]; then
+    echo "Starting the cloud.. "
+    node jamlib.js --conf=cloud.conf --app=ret7 --cloud --port=3883 &
+fi
+
+# Starting the fogs
+
+for port in $(seq 2883 2886);
+do
+    echo "Starting mosquitto at $port"
+    startmosquitto $port 
+    res=$(findmosquitto $port)
+    if [ $res = 1 ]; then
+	echo "Starting a fog.."
+	node jamlib.js --conf=fog$port.conf --app=ret7 --fog --port=$port &
+    fi
+done
+
+# Starting the devices
+
+for port in $(seq 1883 1890);
+do
+    echo "Starting mosquitto at $port"
+    startmosquitto $port 
+    res=$(findmosquitto $port)
+    if [ $res = 1 ]; then
+	echo "Starting a device.."
+	node jamlib.js --conf=dev$port.conf --app=ret7 --port=$port &
+    fi
+done
