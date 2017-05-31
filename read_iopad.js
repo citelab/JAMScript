408847b5d1bbdf93f074948afd87c33003edd3fc
LillyJ <yjiang28@users.noreply.github.com>
2017-05-30 22:02:13 -0400

    Intra-app communication done
    TODO: Inter-app communication

--- read_iopad.js
+++ read_iopad.js
@@ -3,22 +3,35 @@ const JAMIopad = require("./jamiopad.js"),
 	  JAMManager = require("./jammanager.js");
 
 // initialize datasources
-var logger1 = new JAMLogger(JAMManager, "logger1", "fog"),
-	logger2 = new JAMLogger(JAMManager, "logger2", "fog"),
+var logger3 = new JAMLogger(JAMManager, "logger3", "cloud"),
+	logger4 = new JAMLogger(JAMManager, "logger4", "dev"),
 	iopad   = new JAMIopad(JAMManager, "iopad");
 
-var f1 = {
-	showKey: function(ds){
-		console.log(ds.key);
-	}
-},
-	f2 = {
-	addStream: function(ds){
-		ds.addDatastream("newDev");
-		console.log(ds.numDatastream);
-	}
-};
+var stream1 = logger3.addDatastream("devA"),
+	stream2 = logger3.addDatastream("devB"),
+	stream3 = logger4.addDatastream("devC"),
+	stream4 = logger4.addDatastream("devD");
 
-iopad.getDatasource(logger1.key, f1);
+iopad.subscribe(logger3, function(res){
+	console.log("To logger3",res.new_data);
+});
 
-iopad.getDatasource(logger2.key, f2);
+iopad.subscribe(logger4, function(res){
+	console.log("To logger4",res.new_data);
+});
+
+stream1.set_refresh_rate(1500);
+stream2.set_refresh_rate(1500);
+stream3.set_refresh_rate(1500);
+stream4.set_refresh_rate(1500);
+
+var i = 0;
+function add(){
+	stream1.log('App2:A'+i);
+	stream2.log('App2:B'+i);
+	stream3.log('App2:C'+i);
+	stream4.log('App2:D'+i);
+	i++;
+	setTimeout(add, 2000);
+}
+add();
--- write_iopad.js
+++ write_iopad.js
@@ -9,19 +9,31 @@ var logger1 = new JAMLogger(JAMManager, "logger1", "fog"),
 
 var stream1 = logger1.addDatastream("devA"),
 	stream2 = logger1.addDatastream("devB"),
-	stream3 = logger2.addDatastream("devC");
+	stream3 = logger2.addDatastream("devC"),
+	stream4 = logger2.addDatastream("devE");
+
+stream1.set_refresh_rate(1000);
+stream2.set_refresh_rate(1000);
+stream3.set_refresh_rate(1000);
+stream4.set_refresh_rate(1000);
 
 // add datasources to the iopad
-iopad.addDatasource(logger1);
-iopad.addDatasource(logger2);
+iopad.subscribe(logger1, function(res){
+	//console.log("To logger1",res.new_data);
+});
 
+iopad.subscribe(logger2, function(res){
+	console.log("To logger2",res.new_data);
+});
 
-/*
-for(var i=0;i<10;i++){
-	stream1.log('A'+i, undefined);
-	stream2.log('B'+i, undefined);
-	stream3.log('C'+i, undefined);
+var i = 0;
+function add(){
+	stream1.log('App1:A'+i);
+	stream2.log('App1:B'+i);
+	stream3.log('App1:C'+i);
+	stream4.log('App1:D'+i);
+	i++;
+	setTimeout(add, 2000);
 }
-console.log(stream1, stream3.datastream);
-*/
+add();
 
