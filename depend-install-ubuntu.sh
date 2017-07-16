3a70c5a23d83b04160dade3533ac1c369603fb70
catlab <mahes25@gmail.com>
2017-07-16 19:23:43 -0400

    Fixed the script

--- depend-install-ubuntu.sh
+++ depend-install-ubuntu.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Check for Ubuntu OS..
 if !([ -e /etc/lsb-release ] &&
@@ -7,6 +7,8 @@ if !([ -e /etc/lsb-release ] &&
 	      exit
       fi
 
+# Store the script home.. where the "depend-install-ubuntu.sh was located
+scriptdir=$(dirname -- $(readlink -fn -- "$0"))   
 
 
 # Create a temp directory
@@ -24,87 +26,88 @@ sudo apt-get install -y libbsd-dev
 sudo apt-get install -y libavahi-compat-libdnssd-dev
 
 # Check and install..
-if !type clang > /dev/null; then
-sudo apt-get install -y clang
+if (command -v clang > /dev/null); then
+    echo "clang already installed.. skipping install"
 else
-   echo "clang already installed.."
+    sudo apt install -y clang
 fi
 
-if !type g++ > /dev/null; then
-sudo apt-get install -y g++
+if (command -v g++ > /dev/null); then
+    echo "g++ already installed.."
 else
-   echo "g++ already installed.."
-
+    sudo apt-get install -y g++
 fi
 
-if !type cmake > /dev/null; then
-sudo apt-get install -y cmake
+if (command -v cmake > /dev/null); then
+    echo "cmake already installed.."
 else
-   echo "cmake already installed.."
-
+    sudo apt-get install -y cmake
 fi
 
-if !type mosquitto > /dev/null; then
-sudo apt-get install -y mosquitto
+if (command -v mosquitto > /dev/null); then
+    echo "mosquitto already installed.."
 else
-   echo "mosquitto already installed.."
-
+    sudo apt-get install -y mosquitto
 fi
-
 
 # Install latest Node
-if !type node > /dev/null; then
-sudo apt-get install -y python-software-properties
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get install -y nodejs
+if (command -v node > /dev/null); then
+    echo "node already installed.."
 else
-   echo "node already installed.."
-
+    sudo apt-get install -y python-software-properties
+    curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+    sudo apt-get install -y nodejs
 fi
 
 
 # NANOMSG
-if !type nanocat > /dev/null; then
-wget https://github.com/nanomsg/nanomsg/archive/1.0.0.tar.gz
-tar xvzf 1.0.0.tar.gz
-cd nanomsg-1.0.0
-mkdir build
-cd build
-cmake ..
-cmake --build
-ctest -G Debug
-sudo cmake --build . --target install
-sudo ldconfig
+if (command -v nanocat > /dev/null); then
+    echo "nanomsg already installed.."
+else
+    wget https://github.com/nanomsg/nanomsg/archive/1.0.0.tar.gz
+    tar xvzf 1.0.0.tar.gz
+    cd nanomsg-1.0.0
+    mkdir build
+    cd build
+    cmake ..
+    cmake --build
+    ctest -G Debug
+    sudo cmake --build . --target install
+    sudo ldconfig
 fi
 
 cd ../..
 
 
 # CBOR
-if (dpkg -s libcbor 1>/dev/null 2>/dev/null); then
-wget https://github.com/PJK/libcbor/releases/download/v0.4.0/libcbor-0.4.0-Linux.deb
-sudo dpkg -i libcbor-0.4.0-Linux.deb
+qres=$(dpkg -s libcbor 2>/dev/null | grep "Status")
+if [[ $qres = *[!\ ]* ]]; then
+    wget https://github.com/PJK/libcbor/releases/download/v0.4.0/libcbor-0.4.0-Linux.deb
+    sudo dpkg -i libcbor-0.4.0-Linux.deb
 fi
 
 # MQTT
-if ldconfig -p | grep mqtt3a >/dev/null; then
-sudo apt-get install -y libssl-dev                                                           
-git clone https://github.com/eclipse/paho.mqtt.c.git                                         
-cd paho.mqtt.c                                                                               
-make                                                                                         
-sudo make install                                                                            
-cd ..
+qres=$(ldconfig -p | grep mqtt3a)
+if [[ $qres = *[!\ ]* ]]; then
+    sudo apt-get install -y libssl-dev
+    git clone https://github.com/eclipse/paho.mqtt.c.git
+    cd paho.mqtt.c
+    make
+    sudo make install
+    cd ..
 fi
+
 
 # Redis
-if !type redis-server > /dev/null; then
-wget http://download.redis.io/redis-stable.tar.gz
-tar xvzf redis-stable.tar.gz
-cd redis-stable
-make
-sudo make install
+if (command -v redis-server > /dev/null); then
+    echo "Redis already installed"
+else
+    wget http://download.redis.io/redis-stable.tar.gz
+    tar xvzf redis-stable.tar.gz
+    cd redis-stable
+    make
+    sudo make install
 fi
-
 
 echo "Setting up the NPM modules in the user directory..."
 echo
@@ -119,6 +122,10 @@ npm install cbor
 npm install deasync
 npm install json-fn
 npm install mime
+
+cd $scriptdir
+# install the following here..
+
 
 
 
