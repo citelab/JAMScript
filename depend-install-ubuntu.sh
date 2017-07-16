459de5eb75b759734a1b48ad95e25284872791fa
anrl <mahes25@gmail.com>
2017-07-16 16:16:25 +0000

    More fixes

--- depend-install-ubuntu.sh
+++ depend-install-ubuntu.sh
@@ -14,33 +14,47 @@ sudo apt-get install -y libevent-dev
 sudo apt-get install -y libbsd-dev
 
 # Check and install..
-if !test clang > /dev/null; then
+if !type clang > /dev/null; then
 sudo apt-get install -y clang
+else
+   echo "clang already installed.."
 fi
 
-if !test g++ > /dev/null; then
+if !type g++ > /dev/null; then
 sudo apt-get install -y g++
+else
+   echo "g++ already installed.."
+
 fi
 
-if !test cmake > /dev/null; then
+if !type cmake > /dev/null; then
 sudo apt-get install -y cmake
+else
+   echo "cmake already installed.."
+
 fi
 
-if !test mosquitto > /dev/null; then
+if !type mosquitto > /dev/null; then
 sudo apt-get install -y mosquitto
+else
+   echo "mosquitto already installed.."
+
 fi
 
 
 # Install latest Node
-if !test node > /dev/null; then
+if !type node > /dev/null; then
 sudo apt-get install -y python-software-properties
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
+else
+   echo "node already installed.."
+
 fi
 
 
 # NANOMSG
-if !test nanocat > /dev/null; then
+if !type nanocat > /dev/null; then
 wget https://github.com/nanomsg/nanomsg/archive/1.0.0.tar.gz
 tar xvzf 1.0.0.tar.gz
 cd nanomsg-1.0.0
@@ -73,13 +87,24 @@ cd ..
 fi
 
 # Redis
-if !test redis-server > /dev/null; then
+if !type redis-server > /dev/null; then
 wget http://download.redis.io/redis-stable.tar.gz
 tar xvzf redis-stable.tar.gz
 cd redis-stable
 make
 sudo make install
 fi
+
+
+echo "Setting up the NPM modules in the user directory..."
+echo
+cd $HOME
+npm install mqtt
+npm install command-line-args
+npm install random-js
+npm install node-localstorage
+npm install bunyan
+
 
 
 
@@ -89,4 +114,6 @@ echo "All done!"
 echo "Remember to erase the temp_install_src folder!"
 echo 
 echo
+
+
 
