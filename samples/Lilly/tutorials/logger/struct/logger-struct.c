ca6d253885f6634f050d55ca4a9337c53160e170
yjiang28 <yuechun.jiang@mail.mcgill.ca>
2017-08-07 19:21:10 -0400

    Fix logger-struct sample errors

--- samples/Lilly/tutorials/logger/struct/logger-struct.c
+++ samples/Lilly/tutorials/logger/struct/logger-struct.c
@@ -2,24 +2,8 @@
 #include <stdlib.h>
 
 int main() {
-	int low, diff, date;
-	float hum, wind;
-
 	for (int i = 1;; i++) {
-		date = i%31+1,
-		low  = rand()%15+15;
-		diff = rand()%10;
-		hum  = (rand()%100)/100;
-		wind = rand()%25+(rand()%10)/10;
-		MTLWeather = {
-			.date: date,
-			.lowTemperature: low, 
-			.highTemperature: low+diff,
-			.humidity: hum,
-			.wind: wind,
-			.airQuality: "good",
-			.UV: "strong"
-		};
-		usleep(30000);
+		candidate1 = i;
+		sleep(1);
 	}
 }
--- samples/Lilly/tutorials/logger/struct/logger-struct.js
+++ samples/Lilly/tutorials/logger/struct/logger-struct.js
@@ -1,60 +1,54 @@
 jdata{
-	struct weather{
-		float temperature;
-		float humidity;
-		float wind;
-		char* air_quality;
-		char* UV;
-	} MTLWeather as logger;
+	int candidate1 as logger;
 }
 
 var notify = function(key, entry, device){
+	// var name = device.
 	var size = device.size();
-	console.log("Logging the "+size+"-th data "+entry+" into logger "+" key");
+	console.log("Received "+size+" votes");
 
 	var lastData = device.lastData();
-	console.log("Last data received",lastData);
-
+	var timestamp = lastData.timestamp;
 	var lastValue = device.lastValue();
-	console.log("Last value received",lastValue);
+
+	console.log("Vote from "+lastValue+" at "+timestamp);
 
 	var data = device.data();
-
 	var values = device.values();
 
 	if(device.size()>=10){
 		var last10Data = device.n_data(10);
-		console.log("Last 10 data received",last10Data);
+		console.log("Last 10 votes with timestamps:",last10Data);
 	}
 
 	if(device.size()>=10){
 		var last10Values = device.n_values(10);
-		console.log("Last 10 values received",last10Values);
+		console.log("Last 10 votes are from", last10Values);
 	}
-
 
 	if(device.size()>20){
 		// get the 21th value received
 		var aValue = device.get_value_at(20);
+		console.log("The 20th vote is from",aValue);
 	}
 
 	if(device.size()>20){
 		// get the 10 values starting from the 6th one received
 		var someValue = device.get_range_values(5, 10);
+		console.log("The 5th to the 15th votes are from",someValue)
 	}
 
 	// year, month-1, date, hour, minute, secod
-	var today = new Date(2017, 6, 19, 0, 0, 0);
+	var yesterday = new Date(2017, 7, 7, 0, 0, 0);
+	var today = new Date(2017, 7, 8, 0, 0, 0);
 
 	var dataToday = device.dataAfter(today);
 	var valuesToday = device.valuesAfter(today);
 	console.log("Received "+valuesToday.length+" data today");
-	
-	var timestamp = new Date(2017, 6, 20, 0, 0, 0);
 
-	var dataYesterday = device.dataBetween(today, yesterday);
-	var valueYesterday = device.valuesBetween(today, yesterday);
+	var dataYesterday = device.dataBetween(yesterday, today);
+	var valueYesterday = device.valuesBetween(yesterday, today);
 	console.log("Received "+valueYesterday.length+" data yesterday");
 }
 
-score.subscribe(listener);
+candidate1.subscribe(notify);
