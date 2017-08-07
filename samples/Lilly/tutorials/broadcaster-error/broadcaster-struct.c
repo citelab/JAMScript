26da72a11179b4d5babc9136abe01a42570075a0
yjiang28 <yuechun.jiang@mail.mcgill.ca>
2017-08-07 15:36:21 -0400

    Broadcaster sample demonstrating char* error

---
+++ samples/Lilly/tutorials/broadcaster-error/broadcaster-struct.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+	struct basket local1;
+   	struct bag local2;
+	
+	int i=0;
+
+	while (1) {
+		sleep(1);
+		printf("*********i=%d**********\n", i);
+		local1 = myBasket;
+	    local2 = myBag;
+	    printf("---------i=%d----------\n", i);
+		printf("y=%d\n", y);
+		printf("local1.apple=%d local1.pear=%f\n", local1.apple, local1.pear);
+	    printf("local2.pen=%d local2.water=%f local2.book=%d local2.brand=%s\n", local2.pen, local2.water, local2.book);
+	    i++;
+	}
+}
---
+++ samples/Lilly/tutorials/broadcaster-error/broadcaster-struct.js
@@ -0,0 +1,33 @@
+jdata {
+    
+    struct basket{
+        int apple;
+        float pear;
+    } myBasket as broadcaster;
+
+    struct bag{
+        int pen;
+        float water;
+        int book;
+        char* brand;
+    } myBag as broadcaster;
+}
+
+var i=0;
+
+setInterval(function() {
+    
+    myBasket.broadcast({
+        apple: Math.floor(Math.random() * 100),
+        pear : Math.random() * 1000
+    });
+    
+    myBag.broadcast({
+        pen: Math.floor(Math.random() * 10),
+        water: Math.random() * 100,
+        book: Math.random() * 100,
+        brand: "abc"
+    });
+
+    i++;
+}, 1000);
