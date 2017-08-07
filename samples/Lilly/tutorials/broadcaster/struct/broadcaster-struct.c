7f2d3bdc986c4815136308b6124566e7be521690
yjiang28 <yuechun.jiang@mail.mcgill.ca>
2017-08-07 15:35:09 -0400

    broadcaster with error uploaded

--- samples/Lilly/tutorials/broadcaster/struct/broadcaster-struct.c
+++ samples/Lilly/tutorials/broadcaster/struct/broadcaster-struct.c
@@ -13,7 +13,6 @@ int main(int argc, char *argv[]) {
 		local1 = myBasket;
 	    local2 = myBag;
 	    printf("---------i=%d----------\n", i);
-		printf("y=%d\n", y);
 		printf("local1.apple=%d local1.pear=%f\n", local1.apple, local1.pear);
 	    printf("local2.pen=%d local2.water=%f local2.book=%d\n", local2.pen, local2.water, local2.book);
 	    i++;
