41e694859063a9f1783a9cce6ca6f6879f8d2c40
yjiang28 <yuechun.jiang@mail.mcgill.ca>
2017-08-14 09:17:19 -0400

    Some updates to the samples

---
+++ samples/Lilly/tutorials/broadcaster/flow/inflow/bcast-inflow.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]){
+	struct currentTime local;
+
+	while(1){
+		local = myClock;
+		printf("%d-%d-%d %d:%d:%d\n", local.year, local.month, local.date, local.hour, local.minute, local.second);
+		sleep(1);
+	}
+
+	return 0;
+}
--- samples/Lilly/tutorials/logger/flow/flow-simple.c
+++ samples/Lilly/tutorials/logger/flow/flow-simple.c
@@ -2,23 +2,24 @@
 #include <stdlib.h>
 
 int main() {
-	int temp;
+	int low, diff, date;
 	float hum, wind;
 
 	for (int i = 1;; i++) {
-		
-		temp  = rand()%15+15;
+		date = i%31+1,
+		low  = rand()%15+15;
+		diff = rand()%10;
 		hum  = (rand()%100)/100;
 		wind = rand()%25+(rand()%10)/10;
-
 		MTLWeather = {
-			.temperature: temp, 
+			.date: date,
+			.lowTemperature: low, 
+			.highTemperature: low+diff,
 			.humidity: hum,
 			.wind: wind,
 			.airQuality: "good",
 			.UV: "strong"
 		};
-
-		sleep(1);
+		usleep(30000);
 	}
 }
