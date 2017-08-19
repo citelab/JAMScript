f7596859cd3a7e4afd366142deaabcd15e06ba6c
catlab <mahes25@gmail.com>
2017-08-19 19:34:29 -0400

    JConditional fix

--- samples/JCond/fog_condition/jcond.js
+++ samples/JCond/fog_condition/jcond.js
@@ -1,14 +1,14 @@
 jcond {
     fogonly: sys.type == "fog";
-    testdata: avg__x >= 2.0;
 }
 
 jasync {fogonly} function pong() {
-    console.log("pong..");
-    ping();
+    console.log("pong......");
 }
 
-jasync {testdata} function pong2() {
-    console.log("pong..");
-    ping();
-}
+setInterval(()=> {
+    pong();
+
+}, 2000);
+
+
