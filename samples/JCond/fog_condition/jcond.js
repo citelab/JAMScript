4b242c7cc3953e6c23e8950508d3177c7d9a9e0e
catlab <mahes25@gmail.com>
2017-08-20 15:50:03 -0400

    More fixes

--- samples/JCond/fog_condition/jcond.js
+++ samples/JCond/fog_condition/jcond.js
@@ -1,12 +1,25 @@
 jcond {
     fogonly: sys.type == "fog";
+    cloudonly: sys.type == "cloud";
+    devonly: sys.tag == "temp_sensor";
 }
 
 jasync {fogonly} function pong() {
     console.log("pong......");
 }
 
+
+jasync {cloudonly} function pong2() {
+    console.log("pong......");
+}
+
+
+jasync {devonly} function pong3() {
+    console.log("pong......");
+}
+
 setInterval(()=> {
+   console.log("Calling pong...");
     pong();
 
 }, 2000);
