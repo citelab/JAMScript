ab78f613ece1a0713c4aa0041b578761ee2818e1
catlab <mahes25@gmail.com>
2017-08-27 17:44:55 -0400

    Working char * broadcast

--- samples/Lilly/tutorials/broadcaster-error/broadcaster-struct.c
+++ samples/Lilly/tutorials/broadcaster-error/broadcaster-struct.c
@@ -2,20 +2,19 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-	struct basket local1;
-   	struct bag local2;
+   struct basket local1;
+   struct bag local2;
 	
-	int i=0;
+   int i=0;
 
-	while (1) {
-		sleep(1);
-		printf("*********i=%d**********\n", i);
-		local1 = myBasket;
-	    local2 = myBag;
-	    printf("---------i=%d----------\n", i);
-		printf("y=%d\n", y);
-		printf("local1.apple=%d local1.pear=%f\n", local1.apple, local1.pear);
-	    printf("local2.pen=%d local2.water=%f local2.book=%d local2.brand=%s\n", local2.pen, local2.water, local2.book);
-	    i++;
-	}
+   while (1) {
+      sleep(1);
+      printf("*********i=%d**********\n", i);
+      local1 = myBasket;
+      local2 = myBag;
+      printf("---------i=%d----------\n", i);
+      printf("local1.apple=%d local1.pear=%f\n", local1.apple, local1.pear);
+      printf("local2.pen=%d local2.water=%f local2.book=%d local2.brand=%s\n", local2.pen, local2.water, local2.book, local2.brand);
+      i++;
+   }
 }
