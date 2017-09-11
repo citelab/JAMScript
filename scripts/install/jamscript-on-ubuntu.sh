c1b4a603107960c95f6b00daa4b8abfa921b9bf8
catlab <mahes25@gmail.com>
2017-09-11 19:36:30 -0400

    Script chmod fix

--- scripts/install/jamscript-on-ubuntu.sh
+++ scripts/install/jamscript-on-ubuntu.sh
@@ -2,9 +2,13 @@
 
 # Make /usr/lib/node_modules writeable
 
-sudo chmod o+w  /usr/lib/node_modules                                                         
-sudo chmod o+w /usr/bin                                                                      
-sudo chmod o+w /usr/local/share                                                              
+sudo chmod o+w  /usr/lib/node_modules
+sudo chmod o+w /usr/bin
+sudo chmod o+w /usr/local/share
+
+sudo chmod o+w  /usr/local/lib/node_modules
+sudo chmod o+w /usr/local/bin
+
 
 
 
@@ -14,11 +18,11 @@ make clean
 make
 sudo make install
 cd ../mujs2
-make 
+make
 sudo make install
 cd ../..
 
-sudo chmod o+w /usr/local/lib 
+sudo chmod o+w /usr/local/lib
 # Install JAMScript compiler
 npm -g install
 
@@ -34,4 +38,5 @@ sudo chmod o-w /usr/bin
 sudo chmod o-w /usr/local/share
 sudo chmod o-w /usr/local/lib
 
-
+sudo chmod o-w  /usr/local/lib/node_modules
+sudo chmod o-w /usr/local/bin
