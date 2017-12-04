6e1a02bd1e20ebe93f50426887530a3627c0586a
catlab <mahes25@gmail.com>
2017-12-04 12:56:30 -0500

    Callgraph bug fixed

---
+++ samples/J2C/async/cb_test1/cback.c
@@ -0,0 +1,28 @@
+int id; 
+
+int getid();
+
+jasync testcback(char *m, jcallback cb) 
+{
+   printf("=========>>>>%s\n", m);
+   switch (id) 
+     {
+      case 1:
+	     cb("c-msg from 1");
+	
+	break;
+      case 2:
+	     cb("c-msg from 2");
+	break;
+      default:
+	     cb("c-msg from unknown");
+     }
+}
+
+
+int main(int argc, char **argv) 
+{
+   id = getid();
+   printf("My id %d\n", id);
+ 
+}
