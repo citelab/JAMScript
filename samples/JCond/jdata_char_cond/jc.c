1c3534fa0623e63f9bb8571ccb69d676fdbede5b
catlab <mahes25@gmail.com>
2017-12-10 13:24:29 -0500

    JCond integration working

---
+++ samples/JCond/jdata_char_cond/jc.c
@@ -0,0 +1,6 @@
+
+int main(int argc, char **argv) {
+
+    printf("Starting the C node.. \n");
+    return 0;
+}
---
+++ samples/JCond/jdata_char_cond/jc.js
@@ -0,0 +1,42 @@
+
+jdata {
+    char *x as logger;
+    char *y as broadcaster;
+}
+
+jcond {
+    namecheck: x == y;
+    cloudonly: sys.type == "cloud";
+}
+
+jasync {cloudonly && namecheck} function runTheProc(name) {
+    console.log("========= FOUND The " + name + "! ================");
+}
+
+
+jasync {cloudonly} function runAtCloud() {
+
+    console.log("Running at the cloud...");
+    y.broadcast("bucks");
+    runTheProc("bucks");
+
+}
+
+var xlogger = x.getMyDataStream();
+var q = Math.random();
+setInterval(()=> {
+    runAtCloud();
+
+    if (q < 0.3)
+        xlogger.log("bucks");
+    else if (q < 0.6)
+        xlogger.log("bucks");
+    else if (q < 0.9)
+        xlogger.log("magicks");
+    else
+        xlogger.log("raptors");
+
+    console.log("Logged value ", xlogger.lastValue());
+    console.log("Broadcasted value ", y.getLastValue());
+
+}, 1000);
