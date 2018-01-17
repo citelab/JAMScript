ffcdc09bdcc7b32d83d3937bcd3d5a29f29d4374
catlab <mahes25@gmail.com>
2018-01-17 11:05:42 -0500

    Bug fixes

--- tools/pumba_tools.sh
+++ tools/pumba_tools.sh
@@ -1,7 +1,7 @@
 
-
 killrouterpumba() {
-    if [ -z $1]
+
+    if [ -z $1]; then
         local pid=`ps axw | grep pumba | grep router | awk '{split($0,a, " "); print a[1]}'`
     else
         local pid=`ps axw | grep pumba | grep router | grep $1 | awk '{split($0,a, " "); print a[1]}'`
@@ -26,7 +26,7 @@ restartrouterpumba() {
 
 killfogpumba() {
 
-    if [ -z $1]
+    if [ -z $1]; then
         local pid=`ps axw | grep pumba | grep fog | awk '{split($0,a, " "); print a[1]}'`
     else
         local pid=`ps axw | grep pumba | grep fog | grep $1 | awk '{split($0,a, " "); print a[1]}'`
@@ -52,7 +52,7 @@ restartfogpumba() {
 
 killcloudpumba() {
 
-    if [ -z $1]
+    if [ -z $1]; then
         local pid=`ps axw | grep pumba | grep cloud | awk '{split($0,a, " "); print a[1]}'`
     else
         local pid=`ps axw | grep pumba | grep cloud | grep $1 | awk '{split($0,a, " "); print a[1]}'`
