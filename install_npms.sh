f15dc3c9dff9645004b5bfc453ea6e1745b5d12c
catlab <mahes25@gmail.com>
2018-02-15 20:25:58 -0500

    Working JView integration + electron

---
+++ install_npms.sh
@@ -0,0 +1,31 @@
+npm install -g babel-core@6.26.0
+npm install -g babel-loader@6.4.1
+npm install -g babel-plugin-transform-class-properties@6.10.2
+npm install -g babel-plugin-transform-decorators-legacy@1.3.4
+npm install -g babel-preset-es2015@6.9.0
+npm install -g babel-preset-react@6.11.1
+npm install -g babel-preset-stage-0@6.16.0
+
+npm install -g echarts@3.6.1
+npm install -g element-resize-event@2.0.9
+npm install -g express@4.16.2
+npm install -g image-webpack-loader@3.0.0
+
+npm install -g mobx@2.3.7
+npm install -g mobx-react@3.5.1
+npm install -g mobx-react-router@4.0.1
+
+npm install -g react@15.2.1
+npm install -g react-addons-test-utils@15.3.0
+npm install -g react-dom@15.3.0
+npm install -g react-router@4.1.1
+npm install -g react-router-dom@4.1.1
+
+npm install -g terminal-in-react@3.5.2
+npm install -g terminal-kit@1.13.13
+npm install -g webpack@1.15.0
+npm install -g webpack-dev-middleware@1.12.2
+npm install -g webpack-dev-server@1.16.5
+npm install -g webpack-hot-middleware@2.21.0
+
+
