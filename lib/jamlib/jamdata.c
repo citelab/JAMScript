c4858f5c1f0cfa25763cfb419b146d780e457f98
richboy <david.echomgbe@gmail.com>
2018-02-16 03:04:28 -0500

    Fixed bugs

--- lib/jamlib/jamdata.c
+++ lib/jamlib/jamdata.c
@@ -163,13 +163,15 @@ void __jamdata_logto_server(redisAsyncContext *c, char *key, char *val, msg_rcv_
     {
         if (iscbor)
             redisAsyncCommand(c, callback, val, "EVAL %s 1 %s %s", "redis.replicate_commands(); \
-                                                local t = (redis.call('TIME'))[1]; \
+                                                local timeArray = redis.call('TIME'); \
+                                                local t = tonumber(timeArray[1] .. '' .. timeArray[2]); \
                                                 local insert_order =  redis.call('ZCARD', KEYS[1]) + 1; \
                                                 redis.call('ZADD', KEYS[1], t, ARGV[1] .. \"$$$\" .. insert_order .. \"$$$\" .. t .. \"$$$\" .. \"cbor\"); \
                                                 return {t}", key, val);
         else
             redisAsyncCommand(c, callback, val, "EVAL %s 1 %s %s", "redis.replicate_commands(); \
-                                                local t = (redis.call('TIME'))[1]; \
+                                                local timeArray = redis.call('TIME'); \
+                                                local t = tonumber(timeArray[1] .. '' .. timeArray[2]); \
                                                 local insert_order =  redis.call('ZCARD', KEYS[1]) + 1; \
                                                 redis.call('ZADD', KEYS[1], t, ARGV[1] .. \"$$$\" .. insert_order .. \"$$$\" .. t); \
                                                 return {t}", key, val);
