854ba916fd2b3ef123cff7018cb8b05945e490f5
Robert Wenger <robwenger@gmail.com>
2018-08-22 03:34:52 -0400

    Update installer files

--- scripts/install/runtime.sh
+++ scripts/install/runtime.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-
-npm install --prefix ~/__jamruns lib/jamserver
-npm install --prefix ~/__jamruns lib/jdiscovery
-npm install --prefix ~/__jamruns lib/flow
+mkdir -p $HOME/__jamruns/
+ln -s $PWD/node_modules $HOME/__jamruns/node_modules
