e9fc0b9f91c3391e3c995fc145771b1ec3ada6ff
Robert Wenger <robwenger@gmail.com>
2018-09-23 19:23:28 -0400

    Fix npm linking script

--- scripts/install/link.sh
+++ scripts/install/link.sh
@@ -3,10 +3,19 @@
 npm link
 
 rm -rf node_modules/jamserver
-ln -sfn $(pwd)/lib/jamserver node_modules
+cd lib/jamserver
+npm link
+cd ../..
+mv $(npm prefix -g)/lib/node_modules/jamserver node_modules
 
 rm -rf node_modules/jdiscovery
-ln -sfn $(pwd)/lib/jdiscovery node_modules
+cd lib/jdiscovery
+npm link
+cd ../..
+mv $(npm prefix -g)/lib/node_modules/jdiscovery node_modules
 
 rm -rf node_modules/flows.js
-ln -sfn $(pwd)/lib/flow node_modules/flows.js
+cd lib/flow
+npm link
+cd ../..
+mv $(npm prefix -g)/lib/node_modules/flows.js node_modules
