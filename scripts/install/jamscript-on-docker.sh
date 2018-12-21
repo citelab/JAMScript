a46a349919467cf8b87e58757262654210f5aa30
anrl <mahes25@gmail.com>
2018-12-21 03:10:41 -0500

    Modified dockerfile

--- scripts/install/jamscript-on-docker.sh
+++ scripts/install/jamscript-on-docker.sh
@@ -8,35 +8,11 @@ sudo chmod o+w /usr/local/share
 sudo chmod o+w /usr/local/lib
 
 
-# Install the libtask
-cd deps/libtask
-make clean
-make
-sudo make install
-cd ../mujs2
-make
-sudo make install
-cd ../paho
-make
-sudo make install
-cd ../..
-
 # Install JAMScript compiler
-npm -g --unsafe-perm install
+npm -g install jamscript
 
-# Install the other modules
-npm install -g lib/flow
-npm install -g lib/jamserver
-npm install -g lib/jdiscovery
-
-# Reset permissions
 
 sudo chmod o-w /usr/local/lib/node_modules
 sudo chmod o-w /usr/bin
 sudo chmod o-w /usr/local/share
 sudo chmod o-w /usr/local/lib
-
-# This is a hack!
-cd /JAMScript/lib/jamserver
-# There should not be a node_modules directory here
-rm -rf node_modules
