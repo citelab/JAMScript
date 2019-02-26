9790037744726499b7f9b8efa8ec378154899b1f
Matthew Lesko <mlesko1996@gmail.com>
2019-02-25 19:40:44 -0500

    Clear data after each test

--- deps/cellulariot/test/testcellulariot.c
+++ deps/cellulariot/test/testcellulariot.c
@@ -4,6 +4,7 @@ Modification Log:
 Date                  Author                  Modification
 -----------------------------------------------------------------
 03-Dec-2018           Samuel G                Created the file
+25-Feb-2019           Matthew Lk              Clear data after each test
 ==================================================================*/
 
 #include <stdio.h>
@@ -18,34 +19,40 @@ int main() {
         printf("\tRead successful. Data received: %i\n\n", data);
     else
         printf("\tRead failed\n\n");
+    data = 0;
 
     printf("ADC:\n");
     if (read_adc(&data, 1, 1.0f) == 0)
         printf("\tRead successful. Data received: %i\n\n", data);
     else
         printf("\tRead failed\n\n");
+    data = 0;
 
     printf("Ambient light:\n");
     if (read_lux(&data) == 0)
         printf("\tRead successful. Data received: %i\n\n", data);
     else
         printf("\tRead failed\n\n");
+    data = 0;
 
     printf("Temperature:\n");
     if (read_temp(&data) == 0)
         printf("\tRead successful. Data received: %i\n\n", data);
     else
         printf("\tRead failed\n\n");
+    data = 0;
 
     printf("Humidity:\n");
     if (read_hum(&data) == 0)
         printf("\tRead successful. Data received: %i\n\n", data);
     else
         printf("\tRead failed\n\n");
+    data = 0;
 
     printf("GPS:\n");
     if (read_gps(&data) == 0)
         printf("\tRead successful. Data received: %i\n\n", data);
     else
         printf("\tRead failed\n\n");
+    data = 0;
 }
