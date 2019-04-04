e76f0d136514bb1c101a3d1f9d681f1980366b60
Matthew Lesko-Krleza <mlesko1996@gmail.com>
2019-04-03 20:55:46 -0400

    Refactor macros
    Temperature and Humidity measurements are incorrect

--- deps/cellulariot/src/SDL_Pi_HDC1000.c
+++ deps/cellulariot/src/SDL_Pi_HDC1000.c
@@ -16,12 +16,15 @@ Date		    Author		    Modification
 #include <unistd.h>
 
 #define I2C_BUS 1
-#define HDC1080_I2C_ADDRESS 0x40
-#define ACQUISITION_CONFIGURATION_REGISTER_ADDRESS 0x02
-#define TEMPERATURE_MEASUREMENT_ADDRESS 0x00
-#define HUMIDITY_MEASUREMENT_ADDRESS 0x01
 #define I2C_FLAGS 0
 
+#define HDC1080_I2C_ADDRESS 						(char) 0x40
+#define HDC1080_CONFIGURATION_REGISTER 				(char) 0x02
+#define HDC1080_TEMPERATURE_REGISTER 				(char) 0x00
+#define HDC1080_HUMIDITY_REGISTER 					(char) 0x01
+
+#define HDC1080_CONFIG_ACQUISITION_MODE 			(char) 0x10
+#define HDC1080_CONFIG_TEMPERATURE_RESOLUTION_14BIT (short) 0x0000
 /*
 @description: 	Opens and returns a file descriptor for reading temperature and humidity data
 @author: 	Matthew L-K
@@ -41,8 +44,8 @@ int HDC1080_open()
     // Set acquisition mode to independently measure temperature and humidity
     // with 14 bit resolutions
     char config[3] = {0};
-    config[0] = HDC1080_I2C_ADDRESS;
-    config[1] = ACQUISITION_CONFIGURATION_REGISTER_ADDRESS;
+    config[0] = HDC1080_CONFIGURATION_REGISTER;
+    config[1] = HDC1080_CONFIG_ACQUISITION_MODE;
     config[2] = 0x00;
     write(fd, config, 3);
     sleep(0.015); // Time from data sheet
@@ -58,12 +61,12 @@ int HDC1080_open()
 */
 int read_temperature(int fd, int *data)
 {
-    char reg[2] = {0};
-    reg[0] = HDC1080_I2C_ADDRESS + 1; // Last bit must be set high for a read
-    reg[1] = TEMPERATURE_MEASUREMENT_ADDRESS;
-    write(fd, reg, 2);
-    sleep(0.0625); // Time from data sheet
-
+	char config[2] = {0};
+	config[0] = HDC1080_CONFIGURATION_REGISTER;
+	config[1] = HDC1080_TEMPERATURE_REGISTER;
+	write(fd, config, 2);
+	sleep(0.625);
+	
     // Temperature register is a 16-bit result register (the 2 LSBs are always 0)
     char buff[2] = {0};
     if (read(fd, buff, 2) != 2)
@@ -71,9 +74,9 @@ int read_temperature(int fd, int *data)
         printf("Failure HDC1080: Input/Output error. \n");
         return -1;
     }
-
-    short temp = (buff[1] << 8) | buff[0];
-    *data = (temp / 65536) * 165 - 40;
+	
+	short temp = buff[0] * 256 + buff[1];
+    *data = (temp / 65536) * 165 + 25;
     close(fd);
 
     return 0;
@@ -89,7 +92,7 @@ int read_humidity(int fd, int *data)
 {
     char reg[2] = {0};
     reg[0] = HDC1080_I2C_ADDRESS + 1; // Last bit must be set high for a read
-    reg[1] = HUMIDITY_MEASUREMENT_ADDRESS;
+    reg[1] = HDC1080_HUMIDITY_REGISTER;
     write(fd, reg, 2);
     sleep(0.0625);
 
@@ -106,3 +109,22 @@ int read_humidity(int fd, int *data)
 
     return 0;
 }
+
+int read_configuration_register(int fd, short *data)
+{
+	char config[1] = {HDC1080_CONFIGURATION_REGISTER};
+	write(fd, config, 1);
+	sleep(0.0625);
+
+	char buff[2] = {0};
+	if ((read(fd, buff, 2)) != 2)
+	{
+		printf("Failure HDC1080: Read Config Register Error. \n");
+		return -1;
+	}
+	
+	*data = buff[0] * 256 + buff[1];
+
+	return 0;
+}
+
