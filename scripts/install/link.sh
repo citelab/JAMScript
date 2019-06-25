9958b7bca5ce789cf7fad11c58c2a33173beb399
anrl <mahes25@gmail.com>
2019-06-25 14:38:26 +0200

    Working again in mac os

--- scripts/install/link.sh
+++ scripts/install/link.sh
@@ -2,19 +2,19 @@
 
 npm link
 
-rm -rf node_modules/jamserver
+sudo rm -rf node_modules/jamserver
 cd lib/jamserver
 npm link
 cd ../..
 mv $(npm prefix -g)/lib/node_modules/jamserver node_modules
 
-rm -rf node_modules/jdiscovery
+sudo rm -rf node_modules/jdiscovery
 cd lib/jdiscovery
 npm link
 cd ../..
 mv $(npm prefix -g)/lib/node_modules/jdiscovery node_modules
 
-rm -rf node_modules/flows.js
+sudo rm -rf node_modules/flows.js
 cd lib/flow
 npm link
 cd ../..
