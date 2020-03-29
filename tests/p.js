a36bf150df5a5f672b3ad6e940b921798b84066c
anrl <mahes25@gmail.com>
2020-03-29 12:10:53 -0400

    Fixed the bug with downward cloud ASYNC calls

---
+++ tests/p.js
@@ -0,0 +1,23 @@
+jcond {
+    cloudOnly: jsys.type == "cloud";
+    fogOnly: jsys.type == "fog";
+    deviceOnly: jsys.type == "device";
+}
+
+jasync {cloudOnly} function f1() {
+    console.log("Hello from the cloud!")
+}
+
+jasync {fogOnly} function f2() {
+    console.log("Hello from the fog!");
+    f1();
+}
+
+jasync {deviceOnly} function f3() {
+    console.log("Hello from the device!");
+    f2();
+}
+
+if (jsys.type == "device") {
+    setInterval(f3, 2000);
+}
