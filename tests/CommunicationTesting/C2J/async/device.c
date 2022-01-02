4a3e30f1fd865d07a5d68110e3dc107b3b02f10d
IngTian <tzy20001018@gmail.com>
2022-01-01 22:15:57 -0500

    tests: validated tests

--- tests/CommunicationTesting/C2J/async/device.c
+++ tests/CommunicationTesting/C2J/async/device.c
@@ -1,14 +1,13 @@
+#include <stdio.h>
 #include <unistd.h>
 
 void f_param(int d);
 void f_no_param();
 
-int main(int argc, char* argv[])
-{
-    sleep(2);
-    f_param(4);
-    f_no_param();
-    f_param(8);
-    f_no_param();
-    return 0;
+int main(int argc, char *argv[]) {
+  f_param(4);
+  f_no_param();
+  f_param(8);
+  f_no_param();
+  return 0;
 }
