c618760ee355251530e4c5893e824403a16e3960
Zhelin <51770709+J0nnyCheese@users.noreply.github.com>
2022-03-01 12:50:13 -0500

    Fix compatiblity issues related to installing JAMScript on non-MacOS environments

--- install.sh
+++ install.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 
 echo "Installing JAMScript.."
-echo "IMPORTANT: You can be prompted to enter the sudo password.."
-echo 
+# echo "IMPORTANT: You can be prompted to enter the sudo password.."
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
+if [[ $OSTYPE != 'darwin'* ]]; then
+  echo "The local environment is not MacOS. Installing a compatibility layer for mDNS modules..."
+  apt install libavahi-compat-libdnssd-dev
+fi
 cd lib/jamserver 
 npm install 
 cd ../..
 cd lib/jdiscovery
 npm install 
-cd ../..
+cd mdns
+npm link --unsafe-perm
+cd ../../..
 npm install
 npm run link
