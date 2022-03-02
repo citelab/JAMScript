a7dc3f74fbc64e0e634204787eb536f8eb1130a4
Zhelin <51770709+J0nnyCheese@users.noreply.github.com>
2022-03-02 09:30:21 -0500

    Modify the installation process.

--- install.sh
+++ install.sh
@@ -7,17 +7,25 @@ if [[ $UID != 0 ]]; then
     echo "sudo $0 $*"
     exit 1
 fi
+
 if [[ $OSTYPE != 'darwin'* ]]; then
   echo "The local environment is not MacOS. Installing a compatibility layer for mDNS modules..."
   apt install libavahi-compat-libdnssd-dev
 fi
+
 cd lib/jamserver 
 npm install 
 cd ../..
+
 cd lib/jdiscovery
 npm install 
-cd mdns
-npm link --unsafe-perm
-cd ../../..
+
+cd ../..
 npm install
+
+echo "Installing mDNS.."
+cd lib/jdiscovery/mdns
+npm link --unsafe-perm
+
+cd ../../..
 npm run link
