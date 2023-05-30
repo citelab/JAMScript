8aeee5fa72a118c63453076411324e1170c82aaa
anotherLostKitten <eggismagicchickensquared@gmail.com>
2023-05-30 14:15:23 -0400

    script for automatically testing until twheel failure

---
+++ tests/Integration/LOCAL+C2Jrv/twheelquery.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+JAMLOG="${HOME}/.jamruns/apps/jt2_twheelquery/log"
+
+if [[ ! -d "autotest_twheel" ]]; then
+	mkdir "autotest_twheel"
+fi
+
+if [[ ! -f "jt2.jxe" ]]; then
+	jamc jt2.* -d
+fi
+
+while :
+do
+	rm -rf "$JAMLOG"
+	echo "running"
+	timeout 10 jam run "jt2.jxe" --app=twheelquery --verb --log > "autotest_twheel/log_j" 2>&1
+	result=$(grep "message-to-j" "autotest_twheel/log_j")
+	if [ ! -n "$result" ]; then
+		cp "$JAMLOG" "autotest_twheel/log_c"
+		exit
+	fi
+done
