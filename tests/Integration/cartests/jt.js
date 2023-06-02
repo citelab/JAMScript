bfe9b7d866d315806c1fcb66a127962ebb934dea
anotherLostKitten <eggismagicchickensquared@gmail.com>
2023-06-02 14:03:46 -0400

    uncomment jside

--- tests/Integration/cartests/jt.js
+++ tests/Integration/cartests/jt.js
@@ -1,9 +1,8 @@
-//if(jsys.type == "fog") {
-//	setInterval(()=> {
-//		console.log("pinging cars");
-//		carping(10).catch(()=> {
-//			console.log("Error in calling carping...");
-//		});
-//	}, 4000);
-//}
-console.log("test");
+if(jsys.type == "fog") {
+	setInterval(()=> {
+		console.log("pinging cars");
+		carping(10).catch(()=> {
+			console.log("Error in calling carping...");
+		});
+	}, 4000);
+}
