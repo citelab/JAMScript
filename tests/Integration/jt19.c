15bbabe967e61b483265d357b03c764675752c12
TheFishInTheAir <contact@ethanbreit.com>
2023-06-09 15:53:52 -0400

    Device fix

---
+++ tests/Integration/jt19.c
@@ -0,0 +1,30 @@
+char *compyou(char *s);
+void remoteEchoReceiver(char *s, int count);
+
+jtask* {typeAonly} localyou(int c, char *s) {
+    char *y;
+    while(1) {
+        jsleep(100000);
+        printf("############-->>> Hello YOU  %d, %s\n", c, s);
+    //    y = compyou(s);
+    //    printf("---->> Value = %s\n", y);
+    }
+}
+
+jtask int get_a_value(int x) {
+    printf("Value of x %d\n", x);
+    return x;
+}
+
+jtask void remoteEcho(char* s) {
+    static int count = 0;
+    
+    remoteEchoReceiver(s, count++);
+}
+
+int main(int argc, char *argv[])
+{
+ //   localme(10, "cxxxxyyyy");
+    localyou(10, "cxxxxxxxx");
+    return 0;
+}
---
+++ tests/Integration/jt19.js
@@ -0,0 +1,43 @@
+
+jcond {
+    typeAonly: jsys.tag == "typeA";
+    fogonly: jsys.type == "fog";
+}
+
+let count = 10;
+
+jtask function remoteEchoReceiver(str, count) {
+    console.log("Received an echo!");
+    console.log(str, count);
+}
+
+jtask {fogonly} function compyou(str) {
+    count++;
+    console.log("Compyou running.... ",  count, str);
+    let qq = count * count;
+    console.log("Value I am returning..... to the other side == ", qq);
+
+    remoteEcho("Fog to worker communication");
+
+    return qq;
+}
+
+let long = jsys.long;
+let lat = jsys.lat;
+
+setInterval(()=> {
+    console.log("Tag..... ", jsys.tags);
+    if (jsys.type === 'device') {
+        long = (long + 5) % 170;
+        jsys.setLoc({long: long, lat: lat});
+        console.log("Longitude and Latitude .... ", jsys.long, jsys.lat);
+    }
+    remoteEcho("Device to worker communication");
+
+    console.log("Calling compyou.. ");
+    let x = compyou(" String = " + count++); // --> let x = jworklib.machExecuteRV("compyou", ...)
+    x.then((y)=> {console.log(y.status(), y.values())}).catch(()=> {console.log("Helllo.....");});
+
+}, 3000);
+
+
