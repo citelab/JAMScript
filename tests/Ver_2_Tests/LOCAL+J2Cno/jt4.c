70333b8128fce374b4d15e6c0ddfd5732b3ca05b
Mahesh Maheswaran <maheswar@cs.mcgill.ca>
2023-07-06 19:57:37 -0400

    Fixed STOP protocol and deleted unused code

--- tests/Ver_2_Tests/LOCAL+J2Cno/jt4.c
+++ tests/Ver_2_Tests/LOCAL+J2Cno/jt4.c
@@ -3,7 +3,7 @@ char *compyou(char *s);
 
 jtask* localyou(int c, char *s) {
     while(1) {
-        jsleep(1000000);
+        jsleep(10000000);
         printf("############-->>> Hello YOU  %d, %s\n", c, s);
     }
 }
