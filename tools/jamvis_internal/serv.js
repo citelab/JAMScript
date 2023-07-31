d097d5024e346749187c65ac9d2d570adf94d863
TheFishInTheAir <afishintheair@gmail.com>
2023-07-30 20:32:37 -0400

    WIP

--- tools/jamvis_internal/serv.js
+++ tools/jamvis_internal/serv.js
@@ -5,6 +5,8 @@ const express = require('express'),
       fs = require('fs'),
       ws = require('ws'),
       process = require('process'),
+      merc = require('mercator-projection'),
+      cbor = require('cbor-x'),
       mqtt = require('mqtt');
 
 const app = express();
@@ -19,8 +21,8 @@ const app_name = 'jt1';
 /// Try to conenct to local host 
 const global_broker = connect_global_broker();
 
-var brokers = [];
-var broker_nodeid
+///var brokers = [];
+var node_map = new Map();
 
 function connect_global_broker() {
   const global_broker = mqtt.connect("mqtt://127.0.0.1:18830");
@@ -44,8 +46,6 @@ function connect_global_broker() {
   });
 
   global_broker.on("message", (topic, message) => {
-    console.log(topic.toString())
-    console.log(message.toString());
     mqtt_message_handler(topic, message);
   });
   
@@ -54,7 +54,7 @@ function connect_global_broker() {
 
 function connect_broker(url, port) {
   const broker = mqtt.connect(`mqtt://${url}:${port}`);
-  console.log("BRUH: "+ `mqtt://${url}:${port}`);
+  
   broker.on('error', (error) => {
     console.log(error);
   });
@@ -73,9 +73,11 @@ function connect_broker(url, port) {
 }
 
 function register_broker(online_message, node_id, mach_type) {
-
+  console.log("Tracking New Node: "+node_id);
   let internal_mach_type = mach_type;
   let local_registry = false;
+
+  console.log(mach_type);
   if(mach_type == 'local_registry') {
     internal_mach_type = 'fog';
     local_registry = true;
@@ -83,13 +85,26 @@ function register_broker(online_message, node_id, mach_type) {
   
   let entry = {
     broker: connect_broker(online_message.ip, online_message.port),
-    node_id: node_id,
+    node_id: String(node_id),
     loc: online_message.loc,
     mach_type: internal_mach_type,
-    local_registry: local_registry
+    local_registry: local_registry,
+    connections: []
   };
   
-  brokers.push(entry);
+  node_map.set(node_id, entry);
+  update_node_view(entry);
+}
+
+function handle_message_transport(message_data) {
+  let data = cbor.decode(message_data);
+  // receiver/sender is not consistently correct at the moment.
+
+  update_transport_view({
+    receiver: String(data.nodeid),
+    sender: String(data.oldid),
+    task_id: String(data.taskid)
+  });
 }
 
 function mqtt_message_handler(topic, message_text) {
@@ -98,8 +113,9 @@ function mqtt_message_handler(topic, message_text) {
   let node_id      = topic_levels[2];
   let message_type = topic_levels[3];
 
+  // For now...
   if(topic.charAt(0) == '/') {
-//    console.log("meep");
+    handle_message_transport(message_text);
     return;
   }
   
@@ -107,21 +123,78 @@ function mqtt_message_handler(topic, message_text) {
     return;
   
   let message = JSON.parse(message_text);
-  
+
+  // TODO: handle local registry unhost
   if(message_type == 'status') {
     if(message.payload == 'offline') {
-      var to_remove = undefined;
-      let index = 0;
-      for(let broker in brokers) {
-	if(broker.node_id == node_id) {
-	  to_remove = broker;
-	  break;
-	}
-	index++;
+      if(mach_type == 'local_registry') {
+	let node = node_map.get(node_id);
+	if(!node)
+	  return;
+	node.local_registry = false;
+      } else {
+	node_map.delete(node_id);
       }
-      brokers.splice(index, 1);
+
+    } else if (!node_map.has(node_id)) {
+      register_broker(message.payload, node_id, mach_type);
     }
+  } else if (message_type == 'curLoc') {
+    let node = node_map.get(node_id);
+    if(node == undefined)
+      return;
+    node.loc['long'] = message.payload['long'];
+    node.loc['lat'] = message.payload['lat'];
+    update_node_view(node);
+  } else if (message_type == '_debug_selfHost') { // TODO: this seems useless now
+    let node = node_map.get(node_id);
+    if(node == undefined)
+      return;
+    
+    node.local_registry = message.payload;
+    
+  } else if (message_type == '_debug_conns') {
+    let node = node_map.get(node_id);
+    if(node == undefined)
+      return;
+    node.connections = message.payload;
+    update_node_view(node);
   }
+}
+
+function transform_coords(loc) {
+  let xy = merc.fromLatLngToPoint({lat: loc['lat'], lng: loc['long']});
+  let adjusted_xy = {x: xy.x/256.0, y: (xy.y/256.0)+0.5};
+  return adjusted_xy;
+}
+
+function update_node_view(node) {
+  let xy = merc.fromLatLngToPoint({lat: node.loc['lat'], lng: node.loc['long']});
+  let adjusted_xy = {x: xy.x/256.0, y: (xy.y/256.0)+0.5};
+
+  let relevant_info = {
+    node_id: node.node_id,
+    loc_proj: adjusted_xy,
+    loc: node.loc,
+    mach_type: node.mach_type,
+    local_registry: node.local_registry,
+    connections: node.connections
+  };
+
+  let pack = {
+    type: 'state',
+    data: relevant_info
+  };
+  websocketSend(JSON.stringify(pack));
+}
+
+// fnor now, TODO: add more information from here
+function update_transport_view(event_data) {
+  let pack = {
+    type: 'event',
+    data: event_data
+  };
+  websocketSend(JSON.stringify(pack));
 }
 
 
@@ -133,6 +206,21 @@ const websocket_server = new ws.WebSocketServer({port:8681 });
 
 var websockets = [];
 
+function location_average() {
+
+  var acc = {x: 0, y: 0};
+  for(var node_entry of node_map) {
+    var node = node_entry[1];
+    var pos = transform_coords(node.loc);
+    acc = {
+      x: acc.x + pos.x,
+      y: acc.y + pos.y
+    };
+  }
+  return {x: acc.x/node_map.size,
+	  y: acc.y/node_map.size};
+}
+
 function websocketSend(message) {
   for(var websocket of websockets) {
     websocket.send(message);
@@ -143,6 +231,18 @@ websocket_server.on('error', (err)=>{console.error("WS: "); console.error(err)})
 websocket_server.on('connection', function(websocket) {
   console.log("Someone Connected to the websocket.");
   websockets.push(websocket);
+
+  let pack = {
+    type: 'viewport',
+    data: location_average()
+  };
+  websocket.send(JSON.stringify(pack));
+
+  // Bring websocket up to date
+  for(var node_entry of node_map) {
+    var node = node_entry[1];
+    update_node_view(node);
+  }
 });
 
 /////////////////////
@@ -152,7 +252,7 @@ websocket_server.on('connection', function(websocket) {
 const raw_html = `<html><head>
 <script src="https://cdn.jsdelivr.net/npm/pixi.js@7.x/dist/pixi.min.js"></script>
 <script src="sketch.js" defer></script>
-</head><body><h1>Hello PixiJS</h1></body></html>`;
+</head><body style="margin: 0"></body></html>`;
 
 app.get('/', (req, res) => {
   res.send(raw_html);
@@ -160,9 +260,7 @@ app.get('/', (req, res) => {
 
 app.get('/sketch.js', (req, res) => {
   let sketch = fs.readFileSync(`${__dirname}/view.js`,  {encoding: 'utf8', flag: 'r' });
-
   res.send(sketch);
-  setInterval(()=>{websocketSend("BVASDLKJASLDKJASLD!!!")},1000);
 });
 
 app.listen(port, () => {
--- tools/jamvis_internal/view.js
+++ tools/jamvis_internal/view.js
@@ -1,31 +1,371 @@
+
+let node_map = new Map();
+let fogs = [];
+let devices = [];
+var view_scale = 750;
+var viewport_pos = {x:0,y:0};
+var view_offset = {x:0,y:0};
+
+let app, graphics;
+
+let dragging = false;
+let drag_start = {x:0,y:0};
+var view_offset_drag_start = {x:0,y:0};
+
+var remote_calls_observed = 0;
+var remote_calls_observed_text = undefined;
+var devices_text = undefined;
+var fogs_text = undefined;
+
+function scalarAddition(vec2, scale) {
+  return {x: vec2.x+scale, y: vec2.y+scale};
+}
+
+function init_ui(app) {
+
+  var y = 40;
+  const style_large = new PIXI.TextStyle({
+    fontFamily: 'Times New Roman',
+    fontSize: 36,
+    fill: "White"
+  });
+  
+  const style_medium = new PIXI.TextStyle({
+    fontFamily: 'Times New Roman',
+    fontSize: 24,
+    fill: "White"
+  });
+  
+  var text = new PIXI.Text("JAMVis", style_large);
+  text.x = 40;
+  text.y = y;
+  
+  y += 50;
+  app.stage.addChild(text);
+
+
+  remote_calls_observed_text = new PIXI.Text(`Commands Observed: ${remote_calls_observed}`, style_medium);
+  remote_calls_observed_text.x = 40;
+  remote_calls_observed_text.y = y;
+
+  y += 35;
+  app.stage.addChild(remote_calls_observed_text);
+  
+  
+  devices_text = new PIXI.Text(`Devices: ${devices.length}`, style_medium);
+  devices_text.x = 40;
+  devices_text.y = y;
+
+  y += 35;
+  app.stage.addChild(devices_text);
+
+  
+  fogs_text = new PIXI.Text(`Fogs: ${fogs.length}`, style_medium);
+  fogs_text.x = 40;
+  fogs_text.y = y;
+
+  y += 35;
+  app.stage.addChild(fogs_text);
+
+}
+
+
+function update_ui() {
+  remote_calls_observed_text.text = `Calls Observed: ${remote_calls_observed}`;
+  devices_text.text = `Devices: ${devices.length}`;
+  fogs_text.text = `Fogs: ${fogs.length}`;
+}
+
 function setup() {
   let Application = PIXI.Application;
   let Sprite = PIXI.Sprite;
   let Assets = PIXI.Assets;
+  let Ticker = PIXI.Ticker;
   
-  const app = new Application();
+  app = new Application({width: window.innerWidth,
+			 height: window.innerHeight,
+			 resolution: 1,
+			 antialias:true});
   document.body.appendChild(app.view);
- 
-  // This creates a texture from a 'bunny.png' image
-  const graphics = new PIXI.Graphics();
-
-  // Rectangle
-  graphics.beginFill(0xDE3249);
-  graphics.drawRect(50, 50, 100, 100);
-  graphics.endFill();
-
+  graphics = new PIXI.Graphics();
   app.stage.addChild(graphics);
+
+  init_ui(app);
   
   const socket = new WebSocket(`ws://localhost:8681`);
-  socket.addEventListener('message', (event) => handleMqtt(event.data));
+  socket.addEventListener('message', (event) => websocket_receive(event));
+
+  // Event Handlers
+  window.addEventListener("resize", function() {
+    resize(app);
+  });
+
+  // final = view_scale*orig
+  // width = view_scale*virtual_canvas
+  // virtual_width = width/view_scale
+
+  // diff_real = (width/view_scale - width/dview_scale)*view_scale
+  
+  // find difference in virtual canvas and convert into real size
+  
+
+  document.addEventListener("wheel", (event) => {
+    let delta = event.deltaY/8*view_scale*0.001;
+    view_scale -= delta;
+  });
+  
+  document.addEventListener("mousedown", (event) => {
+    dragging = true;
+    drag_start = {x: event.screenX, y: event.screenY};
+    view_offset_drag_start = viewport_pos;
+  });
+
+  document.addEventListener("mouseup", (event) => {
+    dragging = false;
+  });
+  
+  document.addEventListener("mouseleave", (event) => {
+    dragging = false;
+  });
+  
+  document.addEventListener("mousemove", (event) => {
+    if(!dragging)
+      return;
+
+    let current_delta = {x: drag_start.x - event.screenX,
+			 y: drag_start.y - event.screenY};
+    
+    viewport_pos = {x: view_offset_drag_start.x + current_delta.x/view_scale,
+		    y: view_offset_drag_start.y + current_delta.y/view_scale};
+  });
+  
+  let shared_ticker = Ticker.shared;
+  shared_ticker.add((delta) => update(delta));
+}
+
+
+let resizeTimer = undefined;
+function resize(app) {
+  if(resizeTimer) {
+    clearTimeout(resizeTimer);
+  }
+  resizeTimer = setTimeout(()=>{
+    app.renderer.resize(window.innerWidth, window.innerHeight);
+    resizeTimer = undefined;
+  },100);
+}
+
+function websocket_receive(event) {
+  let pack = JSON.parse(event.data);
+  if (pack.type === 'event') {
+    handle_transport_event(pack.data);
+  } else if (pack.type === 'state') {
+    handle_mqtt(pack.data);
+  } else if (pack.type === 'viewport') {
+    handle_viewport_update(pack.data);
+  }
+}
+
+function handle_viewport_update(data) {
+  console.log(data);
+  viewport_pos = data;
+}
+
+function handle_transport_event(data) {
+  remote_calls_observed++;
+  
+  let task_id = data.task_id;
+
+  var device = undefined;
+  var other = undefined
+  let sender = node_map.get(data.sender);
+  let receiver = node_map.get(data.receiver);
+  
+  if(sender == undefined || receiver == undefined) {
+    return;
+  }
+  
+  if(sender.data.mach_type == 'device') {
+    device = sender;
+    other = receiver
+  }
+
+  if(receiver.data.mach_type == 'device') {
+    if(device) {
+      console.log("Invalid Thing????!?!?!?!");
+      return;
+    }
+    device = receiver;
+    other = sender;
+  }
+  
+  if(device == undefined)
+    return;
+  
+  device.graphics.signals[other.data.node_id] = 1;
+}
+
+function handle_mqtt(message) {
+
+  if(!node_map.has(message.node_id)) {
+    switch(message.mach_type) {
+    case 'device':
+      devices.push(message.node_id);
+      break;
+    case 'fog':
+      fogs.push(message.node_id);
+      break;      
+    }
+      node_map.set(message.node_id, {
+	graphics: {
+	  pos: {
+	    x: message.loc_proj.x,
+	    y: message.loc_proj.y
+	  },
+	  signals: {}
+	},
+	data: message
+      });
+  }
+  
+  let node = node_map.get(message.node_id);
+  node.data = message;
+}
+
+function transform_pos(pos) {
+  return {x: (pos.x - viewport_pos.x)*view_scale + window.innerWidth/2,
+	  y: (pos.y - viewport_pos.y)*view_scale + window.innerHeight/2};
+//  return {x: pos.x*view_scale+view_offset.x,
+//	  y: pos.y*view_scale+view_offset.y};
+}
+
+function center_of_square(pos, size) {
+  return {x: pos.x + size/2,
+	  y: pos.y + size/2};
+}
+
+function lerp(v0, v1, a) {
+  return v0 + a * (v1 - v0);
+}
+
+function lerp_colour(col1, col2, a) {
+  return {r: lerp(col1.r, col2.r, a),
+	  g: lerp(col1.g, col2.g, a),
+	  b: lerp(col1.b, col2.b, a)};
+}
+
+const rect_size = 20
+
+function render(delta) {
+  graphics.clear();
+  
+  let default_line_colour = {r: 150, g: 0, b: 0};
+  let default_line_style = {
+    width: 2,
+    color: default_line_colour
+  };
+
+  let signal_line_colour = {r: 255, g: 255, b: 255};
+  
+  graphics.lineStyle(default_line_style);
+  
+  // Connection Pass
+  for(var device of devices) {
+    var node = node_map.get(device);
+    var pos = center_of_square(transform_pos(node.graphics.pos), rect_size);
+    
+    for(var node_id of node.data.connections) {
+      var conn_node = node_map.get(node_id);
+      if(conn_node == undefined)
+	continue;
+      
+      if(conn_node.local_registry)
+	continue;
+
+      let signal = node.graphics.signals[node_id];
+      if(signal) {
+	graphics.lineStyle({
+	  width: 2,
+	  color: lerp_colour(default_line_colour, signal_line_colour, Math.pow(signal, 0.5))
+	})
+      }
+      
+      var conn_pos = center_of_square(transform_pos(conn_node.graphics.pos), rect_size);
+      
+      graphics.moveTo(pos.x, pos.y);
+      graphics.lineTo(conn_pos.x, conn_pos.y);
+
+      if(signal) {
+	graphics.lineStyle(default_line_style);
+      }
+    }
+  }
+
+  graphics.lineStyle({
+    width: 0,
+    color: 0
+  });
+  
+  // Device Pass
+  graphics.beginFill(0xDE3249);
+  for(var device of devices) {
+    var node = node_map.get(device);
+    
+    let pos = transform_pos(node.graphics.pos);
+    
+    graphics.drawRect(pos.x, pos.y, rect_size, rect_size);
+  }
+  graphics.endFill();
+
+
+  // Local Registry Pass
+  graphics.beginFill(0xF044F0);
+  for(var fog of fogs) {
+    var node = node_map.get(fog);
+    if(!node.data.local_registry) {
+      continue;
+    }
+    
+    let pos = transform_pos(node.graphics.pos);
+    
+    graphics.drawRect(pos.x-2, pos.y-2, rect_size+4, rect_size+4);
+  }
+  graphics.endFill();
+  
+  // Fog Pass
+  graphics.beginFill(0x00ff00);
+  for(var fog of fogs) {
+    var node = node_map.get(fog);
+    
+    let pos = transform_pos(node.graphics.pos);
+    
+    graphics.drawRect(pos.x, pos.y, rect_size, rect_size);
+  }
+  graphics.endFill();
+}
+
+function update(delta) {
+
+  // Animate Location Changes
+  for(var node_entry of node_map) {
+    var node = node_entry[1];
+
+    var dx = node.data.loc_proj.x - node.graphics.pos.x;
+    var dy = node.data.loc_proj.y - node.graphics.pos.y;
+
+    node.graphics.pos.x += dx * 0.25 * delta;
+    node.graphics.pos.y += dy * 0.25 * delta;
+
+    for(var signal_node_id in node.graphics.signals) {
+      node.graphics.signals[signal_node_id] -= 0.05 * delta;
+      if(node.graphics.signals[signal_node_id] <= 0) {
+	delete(node.graphics.signals[signal_node_id]);
+      }
+    }
+  }
+  
+  update_ui();
+  render();
 }
 
 setup();
-
-function handleMqtt(message){
-  console.log("Received Message: "+message); 
-}
-  
-function draw() {
-  background(220,0,120);
-}
