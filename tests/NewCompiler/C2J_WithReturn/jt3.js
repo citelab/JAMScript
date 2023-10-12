2d2db3d2fe87d4431141dbfa382f96c1a5d8486a
anotherLostKitten <eggismagicchickensquared@gmail.com>
2023-10-12 02:48:43 -0400

    reuse for the jside

--- tests/NewCompiler/C2J_WithReturn/jt3.js
+++ tests/NewCompiler/C2J_WithReturn/jt3.js
@@ -1,6 +1,12 @@
 let count = 0;
 
-jsync char[60] [reuse = false] compyou(str: char*) {
+jreuse {
+    test(olds, news) {
+        return olds.arg_0 == news.arg_0;
+    }
+}
+
+jsync char[60] [reuse = test, reuse_history = 3] compyou(str: char*) {
     count++;
     console.log(count, str);
     return "hello -- " + count + " " + str;
