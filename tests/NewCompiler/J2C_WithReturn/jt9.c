8b16b655ffdc5a3f570c0efaa89181444a4954d0
anotherLostKitten <eggismagicchickensquared@gmail.com>
2023-10-12 00:55:01 -0400

    default functions on the c side

--- tests/NewCompiler/J2C_WithReturn/jt9.c
+++ tests/NewCompiler/J2C_WithReturn/jt9.c
@@ -2,14 +2,22 @@ jreuse {
     test(old, new) {
         return old.arg_0 == new.arg_0;
     }
+    whatever(old, new) {
+        return true;
+    }
 }
 
 
-jasync localyou(int c, char *s) {
+jasync [reuse_history = 2] localyou(int c, char *s) {
     for(;1;) {
         jsys.sleep(1000000);
         printf("... ############-->>> Hello YOU  %d, %s\n", c, s);
     }
+}
+
+jsync int[30] [reuse = whatever, reuse_history = 5] reuse_array(int q[]) {
+    jarray int i[30] = {1, 3, 5, 6};
+    return i;
 }
 
 jsync int [reuse = test] get_a_value(int x) {
